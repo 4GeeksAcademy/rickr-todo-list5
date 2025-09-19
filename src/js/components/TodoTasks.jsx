d5d3790ca79086063a9b1bee4b34010500901f65
Rick Rodriguez <rickrod6502@gmail.com>
2025-09-19 23:05:43 +0000

    updating TodoTasks.jsx

--- src/js/components/TodoTasks.jsx
+++ src/js/components/TodoTasks.jsx
@@ -1,9 +1,31 @@
+// TodoTasks is the body of the todo list app
+
 
 const TodoTasks = ({todos, setTodos}) => {
 
+    // create a function called deleteTask() that will identify the todo item by id number
+    // and use the .filter() function to create a new array WITHOUT that todo item and display
+    // on the screen
+
+    let renderTasks = todos.map(todo => {
+        return (
+            <li className="list-item" key={todo.id}>
+                <label>{todo.title}</label>
+                {/* create a button that will onClick a deleteTask function to remove the todo item */}
+            </li>
+        );
+    })
+
     return (
         <>
-            {/* use todos.map */}
+            <div className="main">
+                <ul className="todo-list">
+                    {/* I want to render the todo list items if there are 1 or more,
+                    otherwise I want to display "No tasks. Add a task" on the browser." */}
+                    {/* use a ternary that will check the length of the todo list array */}
+                    {todos.length !== 0 ? renderTasks : "No task. Add a task."}
+                </ul>
+            </div>
         </>
     );
 }
